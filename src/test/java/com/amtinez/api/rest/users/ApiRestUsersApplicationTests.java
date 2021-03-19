28ca7be58a0bcedcf0963b384e495b11747e4221
Alejandro Martínez <amartinezcerro90@gmail.com>
2021-03-19 19:40:04 +0100

    ARU-1 Added Spring Boot application

---
+++ src/test/java/com/amtinez/api/rest/users/ApiRestUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amtinez.api.rest.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
