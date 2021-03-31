deb24e357e4ef94c6a90f340879e77469d016a2d
Alejandro Martínez <amartinezcerro90@gmail.com>
2021-03-31 14:02:46 +0200

    ARU-12 Added the user service and it's implementation

---
+++ src/main/java/com/amtinez/api/rest/users/services/impl/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.amtinez.api.rest.users.services.impl;
+
+import com.amtinez.api.rest.users.daos.UserDao;
+import com.amtinez.api.rest.users.models.UserModel;
+import com.amtinez.api.rest.users.services.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Alejandro Martínez Cerro <amartinezcerro @ gmail.com>
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    private UserDao userDao;
+
+    @Override
+    public Optional<UserModel> findUser(final Long id) {
+        return userDao.findById(id);
+    }
+
+    @Override
+    public List<UserModel> findAllUsers() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public UserModel saveUser(final UserModel userModel) {
+        return userDao.save(userModel);
+    }
+
+    @Override
+    public void deleteUser(final Long id) {
+        userDao.deleteById(id);
+    }
+
+    @Override
+    public boolean existsUserEmail(final String email) {
+        return userDao.existsByEmail(email);
+    }
+
+    @Override
+    public int updateUserEnabledStatus(final Long id, final Boolean enabled) {
+        return userDao.updateEnabledStatusById(id, enabled);
+    }
+
+}
