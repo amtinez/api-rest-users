d5468ed39596c1b99e3dff1cb20d8da804f114cb
Alejandro Martínez <amartinezcerro90@gmail.com>
2021-04-01 20:14:35 +0200

    ARU-14 Added role and user mappers

---
+++ src/main/java/com/amtinez/api/rest/users/mappers/RoleMapper.java
@@ -0,0 +1,20 @@
+package com.amtinez.api.rest.users.mappers;
+
+import com.amtinez.api.rest.users.dtos.Role;
+import com.amtinez.api.rest.users.models.RoleModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import static com.amtinez.api.rest.users.constants.MapperConstants.SPRING_COMPONENT_MODEL;
+
+/**
+ * @author Alejandro Martínez Cerro <amartinezcerro @ gmail.com>
+ */
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = SPRING_COMPONENT_MODEL)
+public interface RoleMapper {
+
+    Role roleModelToRole(final RoleModel roleModel);
+
+    RoleModel roleToRoleModel(final Role role);
+
+}
