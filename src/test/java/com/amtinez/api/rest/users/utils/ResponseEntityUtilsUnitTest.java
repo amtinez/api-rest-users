1579dc9a257d987c0f2886430ee113575c1f4a67
Alejandro Martínez <amartinezcerro90@gmail.com>
2021-04-10 13:18:28 +0200

    ARU-17 Added response entity utils and it's unit test

---
+++ src/test/java/com/amtinez/api/rest/users/utils/ResponseEntityUtilsUnitTest.java
@@ -0,0 +1,26 @@
+package com.amtinez.api.rest.users.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Alejandro Martínez Cerro <amartinezcerro @ gmail.com>
+ */
+@ExtendWith(MockitoExtension.class)
+public class ResponseEntityUtilsUnitTest {
+
+    @Test
+    public void testGetResponseEntityByAffectedEntitiesOneEntities() {
+        assertThat(ResponseEntityUtils.getResponseEntityByAffectedEntities(1).getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void testGetResponseEntityByAffectedEntitiesNoEntities() {
+        assertThat(ResponseEntityUtils.getResponseEntityByAffectedEntities(0).getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+}
