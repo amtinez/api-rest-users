416ed7ce638d12a0434b3a1606f5f944db15bbfd
Alejandro Martínez <amartinezcerro90@gmail.com>
2021-04-10 13:20:35 +0200

    ARU-17 Added more constants

--- src/main/java/com/amtinez/api/rest/users/constants/SecurityConstants.java
+++ src/main/java/com/amtinez/api/rest/users/constants/SecurityConstants.java
@@ -10,6 +10,10 @@ public final class SecurityConstants {
     public static final int PASSWORD_LIFETIME_MONTHS = 6;
     public static final int INACTIVE_LIFETIME_MONTHS = 3;
 
+    public static final String HAS_ONLY_ROLE_ADMIN = "hasRole('ROLE_ADMIN')";
+    public static final String HAS_ANY_ROLE = "hasAnyRole('ROLE_ADMIN', 'ROLE_USER')";
+    public static final String PERMIT_ALL = "permitAll()";
+
     private SecurityConstants() {
     }
 
