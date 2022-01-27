a81032d9f9ceabe28b3ae184d24102fadd9f82e6
Alejandro Martínez <amartinezcerro90@gmail.com>
2022-01-27 22:11:55 +0100

    ARU-24 Removed an unnecessary Mockito annotation

--- src/test/java/com/amtinez/api/rest/users/mappers/RoleMapperUnitTest.java
+++ src/test/java/com/amtinez/api/rest/users/mappers/RoleMapperUnitTest.java
@@ -4,10 +4,6 @@ import com.amtinez.api.rest.users.dtos.Role;
 import com.amtinez.api.rest.users.models.RoleModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -17,8 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertNull;
  *
  * @author Alejandro Martínez Cerro <amartinezcerro @ gmail.com>
  */
-@ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
 public class RoleMapperUnitTest {
 
     private static final Long TEST_ROLE_ID = 1L;
