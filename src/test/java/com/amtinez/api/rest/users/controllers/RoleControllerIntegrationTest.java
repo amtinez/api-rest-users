3b8516b47ed5ffd32944dc1f44361800f023736f
Alejandro Martínez <amartinezcerro90@gmail.com>
2022-01-27 22:14:50 +0100

    ARU-24 Modified annotation to make it more generic

--- src/test/java/com/amtinez/api/rest/users/controllers/RoleControllerIntegrationTest.java
+++ src/test/java/com/amtinez/api/rest/users/controllers/RoleControllerIntegrationTest.java
@@ -1,6 +1,6 @@
 package com.amtinez.api.rest.users.controllers;
 
-import com.amtinez.api.rest.users.annotations.WithMockAdminUser;
+import com.amtinez.api.rest.users.annotations.WithMockUser;
 import com.amtinez.api.rest.users.constants.ConfigurationConstants.Profiles;
 import com.amtinez.api.rest.users.dtos.Role;
 import com.amtinez.api.rest.users.models.RoleModel;
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,6 +19,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+
+import static com.amtinez.api.rest.users.constants.SecurityConstants.ROLE_ADMIN;
 
 /**
  * Integration test for {@link RoleController}
@@ -35,7 +36,7 @@ public class RoleControllerIntegrationTest {
     private static final String ROLE_CONTROLLER_URL = "/roles";
 
     private static final String TEST_ROLE_NAME = "testRoleName";
-    private static final String TEST_NOT_EXISTS_ROLE_NAME = "NOT_EXISTS";
+    private static final String TEST_NOT_EXISTS_AUTHORITY_NAME = "NOT_EXISTS";
 
     @Resource
     private MockMvc mockMvc;
@@ -52,7 +53,7 @@ public class RoleControllerIntegrationTest {
     }
 
     @Test
-    @WithMockAdminUser
+    @WithMockUser(authorities = ROLE_ADMIN)
     public void testFindAllRoles() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(ROLE_CONTROLLER_URL))
                .andExpect(MockMvcResultMatchers.status().isOk());
@@ -72,7 +73,7 @@ public class RoleControllerIntegrationTest {
     }
 
     @Test
-    @WithMockAdminUser
+    @WithMockUser(authorities = ROLE_ADMIN)
     public void testRegisterRole() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post(ROLE_CONTROLLER_URL)
                                               .contentType(MediaType.APPLICATION_JSON)
@@ -98,7 +99,7 @@ public class RoleControllerIntegrationTest {
     }
 
     @Test
-    @WithMockAdminUser
+    @WithMockUser(authorities = ROLE_ADMIN)
     public void testUpdateRole() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.put(ROLE_CONTROLLER_URL)
                                               .contentType(MediaType.APPLICATION_JSON)
@@ -107,7 +108,7 @@ public class RoleControllerIntegrationTest {
     }
 
     @Test
-    @WithMockAdminUser
+    @WithMockUser(authorities = ROLE_ADMIN)
     public void testUpdateRoleNotExists() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.put(ROLE_CONTROLLER_URL)
                                               .contentType(MediaType.APPLICATION_JSON)
@@ -133,21 +134,21 @@ public class RoleControllerIntegrationTest {
     }
 
     @Test
-    @WithMockAdminUser
+    @WithMockUser(authorities = ROLE_ADMIN)
     public void testFindRole() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(ROLE_CONTROLLER_URL + "/" + testRole.getId()))
                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
-    @WithMockAdminUser
+    @WithMockUser(authorities = ROLE_ADMIN)
     public void testFindRoleNotExists() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(ROLE_CONTROLLER_URL + "/" + Long.MAX_VALUE))
                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
     @Test
-    @WithMockUser(roles = TEST_NOT_EXISTS_ROLE_NAME)
+    @WithMockUser(authorities = TEST_NOT_EXISTS_AUTHORITY_NAME)
     public void testFindRoleForbidden() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(ROLE_CONTROLLER_URL + "/" + testRole.getId()))
                .andExpect(MockMvcResultMatchers.status().isForbidden());
@@ -160,14 +161,14 @@ public class RoleControllerIntegrationTest {
     }
 
     @Test
-    @WithMockAdminUser
+    @WithMockUser(authorities = ROLE_ADMIN)
     public void testDeleteRole() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete(ROLE_CONTROLLER_URL + "/" + testRole.getId()))
                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
-    @WithMockAdminUser
+    @WithMockUser(authorities = ROLE_ADMIN)
     public void testDeleteRoleNotExists() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete(ROLE_CONTROLLER_URL + "/" + Long.MAX_VALUE))
                .andExpect(MockMvcResultMatchers.status().isNotFound());
