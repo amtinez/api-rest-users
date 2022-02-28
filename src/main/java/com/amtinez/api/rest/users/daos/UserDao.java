dd4c3883549c4143688e1ac3511acc662f25de55
Alejandro Martínez <amartinezcerro90@gmail.com>
2022-02-28 22:46:09 +0100

    ARU-21 Modified find by email and added update password by id

--- src/main/java/com/amtinez/api/rest/users/daos/UserDao.java
+++ src/main/java/com/amtinez/api/rest/users/daos/UserDao.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 /**
  * @author Alejandro Martínez Cerro <amartinezcerro @ gmail.com>
  */
@@ -18,7 +21,7 @@ public interface UserDao extends JpaRepository<UserModel, Long> {
      * @param email the email of the user
      * @return the user if found
      */
-    UserModel findByEmail(final String email);
+    Optional<UserModel> findByEmail(final String email);
 
     /**
      * Retrieves if exists an user with the given email address
@@ -29,10 +32,23 @@ public interface UserDao extends JpaRepository<UserModel, Long> {
     boolean existsByEmail(final String email);
 
     /**
+     * Update the user password if user exists
+     *
+     * @param id       the id of the user
+     * @param password the password of the user
+     * @param date     the date of the update
+     * @return number of updated users
+     */
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE UserModel AS U SET U.password = :password, U.lastPasswordUpdateDate = :date WHERE U.id = :id")
+    int updatePasswordById(@Param("id") final Long id, @Param("password") final String password, @Param("date") final LocalDate date);
+
+    /**
      * Enables or disables the user if it exists
      *
      * @param id      the id of the user
-     * @param enabled the enabled of the user
+     * @param enabled if the user is enabled
      * @return number of updated users
      */
     @Transactional
@@ -44,7 +60,7 @@ public interface UserDao extends JpaRepository<UserModel, Long> {
      * Lock or unlock the user if it exists
      *
      * @param id     the id of the user
-     * @param locked the locked of the user
+     * @param locked if the user is locked
      * @return number of updated users
      */
     @Transactional
