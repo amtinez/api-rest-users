a24fe004767bb74c4c8d8b01607d94fa8b1aa17e
Alejandro Martínez <amartinezcerro90@gmail.com>
2022-02-28 22:39:20 +0100

    ARU-21 Added password reset email group restriction

---
+++ src/main/java/com/amtinez/api/rest/users/validations/groups/PasswordResetEmail.java
@@ -0,0 +1,10 @@
+package com.amtinez.api.rest.users.validations.groups;
+
+/**
+ * Update is the interface created for the password reset email group restriction.
+ *
+ * @author Alejandro Martínez Cerro <amartinezcerro @ gmail.com>
+ */
+public interface PasswordResetEmail {
+
+}
