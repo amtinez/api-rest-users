31323dc50c9dac7767be2a8e4070b5d6a387c3cf
Alejandro Martínez <amartinezcerro90@gmail.com>
2022-02-28 22:46:39 +0100

    ARU-21 Modified implementation

--- src/main/java/com/amtinez/api/rest/users/security/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/amtinez/api/rest/users/security/impl/UserDetailsServiceImpl.java
@@ -2,13 +2,10 @@ package com.amtinez.api.rest.users.security.impl;
 
 import com.amtinez.api.rest.users.daos.UserDao;
 import com.amtinez.api.rest.users.mappers.UserDetailsMapper;
-import com.amtinez.api.rest.users.models.UserModel;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 import javax.annotation.Resource;
 
@@ -26,9 +23,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(final String email) {
-        final Optional<UserModel> user = Optional.ofNullable(userDao.findByEmail(email));
-        return user.map(userDetailsMapper::userModelToUserDetails)
-                   .orElseThrow(() -> new UsernameNotFoundException(String.format("User with the email: %s not found", email)));
+        return userDao.findByEmail(email)
+                      .map(userDetailsMapper::userModelToUserDetails)
+                      .orElseThrow(() -> new UsernameNotFoundException(String.format("User with the email: %s not found", email)));
     }
 
 }
